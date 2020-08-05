8d8b891e467632f161547d4d150f418cf08c71f9
ruy-francisco <ruy.fsalmeida@gmail.com>
2020-08-05 06:21:08 +0100

    New test methods signatures

--- src/Connector/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Connector
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/Tests/GithubJobSearcherTests.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class GithubJobSearcherTests
+    {
+        [Fact]
+        public async Task GetAsync_Should_ReturnListOfJobPositions()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [Fact]
+        public async Task GetAsync_Should_ReturnEmptyList()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+    }
+}
